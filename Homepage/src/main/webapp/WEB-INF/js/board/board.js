66d5663799467ff61df9c1b209e72fae82aec812
nickj <nickj@DESKTOP-0E6S0UO>
2016-02-25 17:52:04 +0900

    json add

--- Homepage/src/main/webapp/WEB-INF/js/board/board.js
+++ Homepage/src/main/webapp/WEB-INF/js/board/board.js
@@ -3,6 +3,33 @@ var $Grid = {};
 $(function(){
 	$Grid = $("#myGrid");
 	$Grid.jqGrid({
-		
+		url: 'js/json/test.json',
+		datatype: "json",
+		mtype: "get",
+		jsonReader: { 
+			id: "id",
+			root: "employee"
+		},
+		colNames:[
+		          '아이디',
+		          '이름',
+		          '나이',
+		          '성별',
+		          '직위'
+		          ],
+		colModel: [
+		           {name:"id", width:40, align:"center"},
+		           {name:"name", width:80, align:"left"},
+		           {name:"age", width:80, align:"left"},
+		           {name:"sex", width:80, align:"right"},
+		           {name:"position", width:50, align:"right"}
+		           ],
+        pager:"#pager",
+        rowNum: "10",
+        multiselect: true,
+        postData: {
+        	id: "id",
+        	name: "name"
+        }
 	});
 });
